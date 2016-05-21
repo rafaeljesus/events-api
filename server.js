9c389cd581a7d18ec9d8fc0b3b025bf186588f1c
Rafael Jesus <rafaelljesus86@gmail.com>
2016-05-21 12:25:33 -0300

    Sync

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import micro, { send, sendError } from 'micro'
 import { parse } from 'url'
+import { wrap, log } from '@rafaeljesus/events-util'
 import Event from '@rafaeljesus/events-core'
 
 const port = process.env.PORT || 3000
@@ -9,19 +10,15 @@ const search = async (req) => {
   return await Event.search(query)
 }
 
-const srv = micro(async (req, res) => {
-  try {
-    switch (req.method) {
-      case 'GET':
-        return send(res, 200, await search(req))
-      default:
-        send(res, 405, 'Invalid method')
-    }
-  } catch (err) {
-    sendError(req, res, err)
+const srv = micro(wrap(async (req, res) => {
+  switch (req.method) {
+    case 'GET':
+      return send(res, 200, await search(req))
+    default:
+      send(res, 405, 'Invalid method')
   }
-})
+}))
 
 srv.listen(port)
 
-console.log(`> \u001b[96mReady!\u001b[39m Listening on ${port}.`)
+log.info(`> \u001b[96mReady!\u001b[39m Listening on ${port}.`)
